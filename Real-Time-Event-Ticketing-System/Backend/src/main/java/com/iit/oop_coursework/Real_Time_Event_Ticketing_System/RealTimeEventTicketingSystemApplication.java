4269e7d326a815ed2d0c559857cd3c6d8585de8b
binuthivithya <binu.20232034@iit.ac.lk>
2024-11-30 19:41:29 +0530

    initial commit

---
+++ Real-Time-Event-Ticketing-System/Backend/src/main/java/com/iit/oop_coursework/Real_Time_Event_Ticketing_System/RealTimeEventTicketingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.iit.oop_coursework.Real_Time_Event_Ticketing_System;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealTimeEventTicketingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealTimeEventTicketingSystemApplication.class, args);
+	}
+
+}
---
+++ Real-Time-Event-Ticketing-System/Backend/src/test/java/com/iit/oop_coursework/Real_Time_Event_Ticketing_System/RealTimeEventTicketingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iit.oop_coursework.Real_Time_Event_Ticketing_System;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealTimeEventTicketingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
